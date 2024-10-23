0f5e78659c7442221ba10ffd6214f5b6d0eec4b7
Hysha Dadwal <160382583+HyshaDadwal@users.noreply.github.com>
2024-10-23 20:56:25 +0530

    Add files via upload

---
+++ quiz_data.py
@@ -0,0 +1,52 @@
+quiz_data = [
+    {
+        "question": "Question 1: What is the largest planet in our solar system?",
+        "choices": ["Jupiter", "Saturn", "Mars", "Earth"],
+        "answer": "Jupiter"
+    },
+    {
+        "question": "Question 2: What is the chemical symbol for gold?",
+        "choices": ["Go", "Au", "Ag", "Gd"],
+        "answer": "Au"
+    },
+    {
+        "question": "Question 3: Which country is known as the 'Land of the Rising Sun'?",
+        "choices": ["China", "Japan", "South Korea", "Thailand"],
+        "answer": "Japan"
+    },
+    {	
+        "question": "Question 4: Which is the largest ocean on Earth?",	
+        "choices": ["Atlantic Ocean", "Pacific Ocean", "Arctic Ocean", "Indian Ocean"],
+        "answer": "Pacific Ocean"
+    },
+    {	
+        "question": "Question 5: What is the number of periods in the Periodic Table?",	
+        "choices": ["6", "7", "8", "9"],
+        "answer": "7"
+    },
+    {	
+        "question": "Question 6: What is the longest river on Earth?",
+        "choices": ["Amazon River", "Nile River", "Yamuna River", "Bhramaputra River"],
+        "answer": "Nile River"
+    },
+    {	
+        "question": "Question 7: What is the currency of Norway?",	
+        "choices": ["Ruble", "Dollar", "Euro", "Krone"],	
+        "answer": "Krone"
+    },
+    {	
+        "question": "Question 8: When did India get independnce?",
+        "choices": ["26th January,1950", "15th August,1947", "15th January,1947", "26th August,1950"],	
+        "answer": "15th August,1947"
+    },
+    {	
+        "question": "Question 9: Who is known as the 'Father of the Indian Constitution?",
+        "choices": ["Dr. B R Ambedkar", "Rajendra Prasad", "Dadabhai Naoroji", "Rabindranath Tagore"],	
+        "answer": "Dr. B R Ambedkar"
+    },
+    {	
+        "question": "Question 10: Who is the author of the famous book series 'Harry Potter'?",
+        "choices": ["Agatha Christie", "Stephen King", "Ernest Hemingway", "J K Rowling"],	
+        "answer": "J K Rowling"
+    }
+]
